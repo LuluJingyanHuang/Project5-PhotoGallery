bf7ea63a0713dc1c6d769f73e18258d64db4bdb7
LuluJingyanHuang <LuluJingyanHuang@gmail.com>
2019-10-03 23:20:22 -0400

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-$('data-lightbox').option({
-      'alwaysShowNavOnTouchDevices':true
-    })
+function getPhoto() {
+  var getName = document.getElementsByTagName("a").toUpperCase();
+  return getName;
+}
+
+console.log(getPhoto());
+
+
+$('input').on('keyup',function(){
+    let result = $('input').val().toLowerCase();
+    console.log(result);
+
+});
+
+$('.photo').append('ahhhhhhhh');
