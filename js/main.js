6775f39de6877a29d6a079a1dcd7ebcf9fe45f62
LuluJingyanHuang <LuluJingyanHuang@gmail.com>
2019-10-06 21:12:50 -0400

    almost there

--- js/main.js
+++ js/main.js
@@ -1,15 +1,53 @@
-function getPhoto() {
-  var getName = document.getElementsByTagName("a").toUpperCase();
-  return getName;
+var result;
+function getID() {
+    var result = document.getElementById('search');
+    result=result.value.toLowerCase()
+    console.log(result);
+    var images = document.querySelectorAll('.photo img');
+    console.log(images.length);
+
+    for (i=0; i<images.length; i+=1) {
+        var image = document.getElementsByTagName('img')[i];
+        var long = document.getElementsByTagName('a')[i];
+        var caption= image.getAttribute('alt').toLowerCase();
+        var longCaption= long.getAttribute('data-title').toLowerCase();
+        var match = result.includes(caption);
+        var match1= longCaption.includes(result);
+        console.log(match1);
+        if (match === true){
+          image.style.display= '';
+
+        } else if (match1 === true){
+            image.style.display= '';
+            console.log(match1);
+        }else {
+          image.style.display = 'none';
+          long.style.display= 'none' ;
+        }
+
+          }
 }
 
-console.log(getPhoto());
+document.getElementById("search").addEventListener("keyup", getID);
 
-
-$('input').on('keyup',function(){
-    let result = $('input').val().toLowerCase();
-    console.log(result);
-
-});
-
-$('.photo').append('ahhhhhhhh');
+//
+// function getCaption() {
+//   var images = document.getElementsByTagName('img');
+//   console.log(images.length);
+//
+//   for (i=0; i<images.length; i+=1) {
+//       var image = document.getElementsByTagName('img')[i];
+//       var caption= image.getAttribute('alt').toLowerCase();
+//       console.log(caption);
+//       console.log(result);
+//       var match = result.includes(caption);
+//       if (match = true){
+//         document.write('aye');
+//           }
+//
+//         }
+//       }
+//
+//
+//
+// getCaption();
